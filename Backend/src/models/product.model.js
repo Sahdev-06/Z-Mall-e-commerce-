f0e51665551c981d179061b8066340c16ecb0625
Sahdev <kumarsahdev315@gmail.com>
2026-05-31 23:02:28 +0530

    add required validation for image field in product schema

--- Backend/src/models/product.model.js
+++ Backend/src/models/product.model.js
@@ -17,7 +17,8 @@ const productSchema = new mongoose.Schema(
         },
         image : [
             {
-                type : String
+                type : String,
+                required : true
             }
         ],
         discount : {
