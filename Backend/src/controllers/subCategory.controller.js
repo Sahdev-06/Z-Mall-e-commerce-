bd7a2c9aefb9860e2128533dfe161e79c21cc9af
Sahdev <kumarsahdev315@gmail.com>
2026-06-18 15:12:45 +0530

    fix deleteSubCategory response to return null for deleted item

--- Backend/src/controllers/subCategory.controller.js
+++ Backend/src/controllers/subCategory.controller.js
@@ -96,7 +96,7 @@ const deleteSubCategory = asyncHandler(async (req, res) => {
     return res
     .status(200)
     .json(
-        new ApiResponse(200, "SubCategory deleted Successfully")
+        new ApiResponse(200, null, "SubCategory deleted Successfully")
     )
 })
 
