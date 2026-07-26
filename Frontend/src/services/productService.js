ac2a5d3d0bee60a5193731ba4915783db5637c00
Sahdev <kumarsahdev315@gmail.com>
2026-07-26 23:51:01 +0530

    implement product service with CRUD operations for admin and public APIs

---
+++ Frontend/src/services/productService.js
@@ -0,0 +1,54 @@
+import axiosInstance from "../api/axios.js"
+
+// admin api
+const createProduct = async (productData) => {
+    const response = await axiosInstance.post("/product/create", productData)
+    return response.data
+}
+
+const getAllProductsForAdmin = async () => {
+    const response = await axiosInstance.get("/product/all")
+    return response.data
+}
+
+const getProductByIdForAdmin = async (id) => {
+    const response = await axiosInstance.get(`/product/get/${id}`)
+    return response.data
+}
+
+const updateProduct = async (id, productdata) => {
+    const response = await axiosInstance.patch(`/product/update/${id}`, productdata)
+    return response.data
+}
+
+const updateProductStock = async (id, stockData) => {
+    const response = await axiosInstance.patch(`/product/${id}/stock`, stockData)
+    return response.data
+}
+
+const deleteProduct = async (id) => {
+    const response = await axiosInstance.delete(`/product/delete/${id}`)
+    return response.data
+}
+
+// public api
+const getAllProducts = async () => {
+    const response = await axiosInstance.get("/product/get-all-product")
+    return response.data
+}
+
+const getProductById = async (id) => {
+    const response = await axiosInstance.get(`/product/get-product/${id}`)
+}
+
+
+export {
+    createProduct,
+    getAllProductsForAdmin,
+    getProductByIdForAdmin,
+    updateProduct,
+    updateProductStock,
+    deleteProduct,
+    getAllProducts,
+    getProductById,
+}
