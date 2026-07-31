9a587e98a5181d4f58db5c20e42b7a48626aca81
Sahdev <kumarsahdev315@gmail.com>
2026-07-31 23:36:17 +0530

    add: restructure SectionHeading component to include navigation for viewing all products

--- Frontend/src/components/Common/SectionHeading.jsx
+++ Frontend/src/components/Common/SectionHeading.jsx
@@ -1,10 +1,29 @@
+import { useNavigate } from "react-router-dom"
 
-function SectionHeading({ title }) {
+function SectionHeading({ title, showViewAll, type }) {
+    const navigate = useNavigate();
+
+    function handleNavigate() {
+        navigate(`/products/${type}`)
+    }
+
     return (
         <>
-            <h1 className="text-3xl font-bold text-slate-900 mb-6">
-                {title}
-            </h1>
+            <div className="flex items-center justify-between">
+                <h1 className="text-3xl font-bold text-slate-900 mb-6">
+                    {title}
+                </h1>
+                {
+                    showViewAll && (
+                        <button
+                            className="text-orange-500 hover:underline cursor-pointer"
+                            onClick={handleNavigate}
+                        >
+                            view all
+                        </button>
+                    )
+                }
+            </div>
         </>
     )
 }
