e10b876cbf6a0012d9f59b15d1951e72ec98db9c
Sahdev <kumarsahdev315@gmail.com>
2026-09-08 19:52:27 +0530

    fix: Add sameSite attribute to cookie options for enhanced security

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -154,7 +154,8 @@ const logoutUser = asyncHandler(async (req, res) => {
 
     const options = {
         httpOnly : true,
-        secure : true
+        secure : true,
+        sameSite : "none"
     }
 
     return res
