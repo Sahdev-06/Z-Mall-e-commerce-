e05e531cbfbd08e42d201cf83167891a5701d53f
Sahdev <kumarsahdev315@gmail.com>
2026-09-08 13:26:05 +0530

    feat: Update environment configuration for axios base URL and enhance .gitignore for environment files

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL : "http://localhost:3000/api/v1",
-    // baseURL : "http://192.168.1.4:3000/api/v1",
+    baseURL : import.meta.env.VITE_API_URL,
     withCredentials : true
 })
 
