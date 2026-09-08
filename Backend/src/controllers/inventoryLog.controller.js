fcc0968c221d68db689f94487735739ecaa21305
Sahdev <kumarsahdev315@gmail.com>
2026-09-08 14:18:47 +0530

    fix: Correct import path casing for ApiError and ApiResponse in inventoryLog controller

--- Backend/src/controllers/inventoryLog.controller.js
+++ Backend/src/controllers/inventoryLog.controller.js
@@ -1,6 +1,6 @@
 import { asyncHandler } from '../utils/asyncHandler.js';
-import { ApiError } from '../utils/apiError.js';
-import { ApiResponse } from '../utils/apiResponse.js';
+import { ApiError } from '../utils/ApiError.js';
+import { ApiResponse } from '../utils/ApiResponse.js';
 import { InventoryLog } from '../models/inventoryLog.model.js'
 import { Product } from '../models/product.model.js'
 import mongoose from 'mongoose';
